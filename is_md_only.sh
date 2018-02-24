8edc97cd8e3fb221ec49136a1f47d8239f34aa90
James C. Davis <james@cos.io>
2018-02-24 01:32:41 -0500

    simplify

--- is_md_only.sh
+++ is_md_only.sh
@@ -2,19 +2,10 @@
 
 set -e
 
-MD_ONLY==true
-
 CHANGED_FILES=`git diff --name-only master...${TRAVIS_COMMIT}`
 
 for CHANGED_FILE in $CHANGED_FILES; do
   if ! [[ $CHANGED_FILE =~ .md$ ]]; then
-    MD_ONLY=false
-    break
+    exit 1
   fi
 done
-
-if [[ $MD_ONLY == true ]]; then
-  exit 0
-else
-  exit 1
-fi
